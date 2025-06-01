e2e1a07f4dfb80ddfa831623d59d909b7655a9bf
6A 11 Aman Sharma <amansharmakyu@gmail.com>
2025-06-01 14:58:02 +0000

    sync it whe nchanging t olight or t odar kmode

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border bg-card text-card-foreground shadow-xl dark:shadow-md dark:shadow-black/40",
       className
     )}
     {...props}
